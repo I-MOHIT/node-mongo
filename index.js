3d4247b06394cf710504b379100ef98acc34b376
I-MOHIT <mohitchugh180919@gmail.com>
2021-11-07 17:01:45 +0530

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'confusion';
@@ -11,6 +12,29 @@ MongoClient.connect(url, (err,client) => {
     console.log("Connected correctly to the server");
 
     const db = client.db(dbname);
+    
+    dboper.insertDocument(db,{"name":"Donut","description":"Yummy"},"dishes",(result) => {
+        console.log('Insert Document:\n',result.ops);
+
+        dboper.findDocuments(db,"dishes",(docs) => {
+            console.log('Found Documents:\n',docs);
+
+            dboper.updateDocument(db,{"name":"Donut"},{"description":"Updated Yummy"},"dishes",(result) => {
+                console.log("Updated Document:\n",result.result);
+
+                dboper.findDocuments(db,"dishes",(docs) => {
+                    console.log('Found Documents:\n',docs);
+
+                    db.dropCollection("dishes",(result) => {
+                        console.log('Dropped Collection:\n',result);
+                    });
+                });
+            });
+        });
+    });
+     
+
+    /*
     const collection = db.collection('dishes');
 
     collection.insertOne({"name":"Pasta","description":"Tasty"}, (err,result) => {
@@ -31,5 +55,5 @@ MongoClient.connect(url, (err,client) => {
                 client.close();
             });
         });
-    });
+    });*/
 });
